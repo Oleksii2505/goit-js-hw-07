2601601ff219fe3847fef244fb1520695463f4b9
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-02-11 20:49:12 +0200

    Hometask1

---
+++ js/01-gallery.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const imageList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", imageList);
+
+console.log(galleryItems);
